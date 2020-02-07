068ce23e86cdc3a7238b45d03e80df48471c81f7
[kamleshOnGit] <[master.kamlesh@outlook.com]>
2020-02-07 16:41:26 +0530

    Escape

--- notes-app/src/app/note-list/note-list.component.ts
+++ notes-app/src/app/note-list/note-list.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class NoteListComponent implements OnInit {
 
   list = [
-    {title: 'Just adding note' ,  body: 'Trying to add some note'} ,
-    {title: 'Study angular' ,  body: 'Study angular'} ,
-    {title: 'Revise angular' ,  body: 'Revise angualr thoroghly'}
+    {title: 'Just adding note' ,  body: 'Trying to add some note' , time : new Date().setTime(34352335)} ,
+    {title: 'Study angular' ,  body: 'Study angular' , time :new Date().setTime(797335)} ,
+    {title: 'Revise angular' ,  body: 'Revise angualr thoroghly' , time : new Date().setTime(5454335)}
   ] ;
+
+  todayDate: Date = new Date();
 
   constructor() { }
 
