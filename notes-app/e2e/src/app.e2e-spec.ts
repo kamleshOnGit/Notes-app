5987e8aed05e454b52c1ca3436a7a6a66c149ff0
[kamleshOnGit] <[master.kamlesh@outlook.com]>
2020-02-07 12:59:09 +0530

    restart

--- notes-app/e2e/src/app.e2e-spec.ts
+++ notes-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to note-app!');
+    expect(page.getTitleText()).toEqual('Welcome to notes-app!');
   });
 
   afterEach(async () => {
